2ca230ca1a67a5da2116af24796a323c4a2d2ee1
Jys <onlys2545@naver.com>
2023-11-22 17:21:31 +0900

    xml 파일 설정

---
+++ my_bank_app/src/main/java/com/tenco/bankapp/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.tenco.bankapp.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TestController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
